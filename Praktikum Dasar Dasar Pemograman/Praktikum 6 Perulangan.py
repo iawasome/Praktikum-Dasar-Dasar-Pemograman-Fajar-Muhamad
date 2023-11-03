a16cae3d6d481361129defaac0cd54827caf4e7e
Fajar Muhamad <146068460+iawasome@users.noreply.github.com>
2023-11-03 21:44:31 +0700

    Add files via upload

---
+++ Praktikum Dasar Dasar Pemograman/Praktikum 6 Perulangan.py
@@ -0,0 +1,93 @@
+#a = 0
+#while a <= 3:
+#    print(a)
+#    a+=1
+
+#a = 0
+#while a <= 10:
+#    print(a)
+ #   a+=2
+
+
+#b = 1
+#while b <= 100:
+#    print("STT Nurul Fikri loop ke-", b)
+#   b+=1
+
+#a = 99
+#while a >= 1:
+#    print(a)
+#    a-=2
+
+#j = 1
+#while(j<=10):
+#    k=1
+#    while(k<=10):
+#        print(j*k, end="")
+#        k+=1
+#    print()
+#    j+=1
+
+#for i in range (50,0,-5):
+#    print(i, end=" ")
+
+#for i in range (3):
+#    print("TI02")
+
+#for i in range(1,11):
+#    for j in range(1,11):
+#        k=i*j
+#        print(k, end=" ")
+#    print()
+
+#i = 1
+#while 1 < 6:
+#    print(i)
+#    if i == 3:
+#        break
+#    i += 1
+
+#i = 0
+#while i < 6:
+#    i+=1
+#    if i == 3:
+#        continue
+#    print(i)
+
+#i = 1
+#while i < 6:
+#    print(i)
+#    i+=1
+
+
+numbers = [951, 402, 984, 651, 360, 69, 408, 319, 601, 485, 980, 507, 725, 547, 544,  615, 83, 165, 141, 501, 263, 617, 865, 575, 219, 390, 984, 592, 236, 105, 942, 941,  386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345,  399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217, 815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717, 958, 609, 842, 451, 688, 753, 854, 685, 93, 857, 440, 380, 126, 721, 328, 753, 470, 743, 527]
+i = 1  # Inisialisasi indeks awal
+
+while i < len(numbers):
+    if numbers[i] % 2 != 0:
+         print(numbers[i])
+         if numbers[i] == 553:
+            break 
+    i += 1
+print(" ")
+
+#Buat lah output dari menggunakan bahasa pemprograman python dengan : 
+#1 + 3 + 5 + 7 +9 +11 +13 + 15 +17 +19 = ….
+
+a = 0
+for i in range (1,20,2):
+      a+=i
+print("1+3+5+7+9+11+13+15+17+19=",a)
+
+#Buat program untuk minta input jumlah baris dan buat
+#rangkaian berikut ini
+#*
+#**
+#***
+#****
+#Dan seterusnya sejumlah baris yang diinputkan
+#Gunakan for loop dengan range
+
+jumlah_baris = int(input("Masukan jumlah baris :"))
+for i in range(1, jumlah_baris + 1):
+    print('*' * i)
